df707f3abb92a846319aa6f5884983e0dc76d07b
Santiago Rada <santynrada@gmail.com>
2025-05-07 15:36:15 -0300

    heroes

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const app = express();
 
 const PORT = process.env.PORT ?? 3005;
 
+const heroes = require('./heroes.json');
+
 app.get('/', (req, res) => {
     res.status(200).send({ message: "Hola mundo" })
 })
+
+app.get('/heroes', (req, res) => {
+    res.status(200).json(heroes);
+});
+
+app.get('/heroes/:id', (req, res) => {
+    const { id } = req.params;
+
+    const hero = heroes.find(h => h.id == id);
+
+    if (hero != null) { res.status(200).json(hero); }
+    else { res.status(404).send({ message: "No se encuentro el heroe" }); }
+});
 
 const server = app.listen(PORT, () => {
     console.log(`El servidor está escuchando en el puerto http://localhost:${server.address().port}`);
